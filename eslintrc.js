842b260a358fb6cb99f24f8e6233f1a746aedb1a
Bayathy <misosoup0321@icloud.com>
2023-02-24 18:22:41 +0900

    feat: change config

--- eslintrc.js
+++ eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "next/core-web-vitals",
+    "next",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:unicorn/recommended",
@@ -54,6 +54,18 @@ module.exports = {
       { prefer: "type-imports" },
     ],
     "@typescript-eslint/no-unused-vars": ["error", { args: "all" }],
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
   },
 };
 
